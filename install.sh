72a2778f746cd022d174587959d8eff88b63e911
Noapila <noehsebille@gmail.com>
2026-09-05 19:35:46 +0200

    updated

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "==> Installing Nopile"
-mkdir -p /var/lib/nopile || exit 1
-mkdir -p /var/tmp/nopile || exit 1
-mkdir -p /var/cache/nopile || exit 1
-cp ./nopile /usr/bin/nopile || exit 1
-echo "Nopile installed Succesfully !"
