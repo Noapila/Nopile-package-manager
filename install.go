aba92cb73a22a4b7b4d896c4e48746cccfc0e87f
Noapila <noehsebille@gmail.com>
2026-09-05 21:14:23 +0200

    add DEBUG

--- install.go
+++ install.go
@@ -174,9 +174,11 @@ func installBinary(name string, force bool, reinstall bool) bool {
 		fmt.Println(ColorRed + "error:", ColorReset + ".nopile not found in package")
 		return false
 	}
+	fmt.Printf("DEBUG: writing .nopile to %s\n", NOPILE_PKG_DIR+"/"+name+".nopile")
 	err = os.WriteFile(NOPILE_PKG_DIR+"/"+name+".nopile", data, 0644)
 	if err != nil {
-		fmt.Println(ColorRed + "error:", ColorReset + "failed to install .nopile")
+		fmt.Printf("DEBUG: write failed: %v\n", err)
+		fmt.Println(ColorRed + "fatal:", ColorReset + "failed to install .nopile")
 		return false
 	}
 
